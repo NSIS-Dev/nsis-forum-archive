df92a2f5d11d8371fc8d126465b862f1c1f72b47
Jan T. Sott <jan@idleberg.com>
2017-06-05 22:27:47 +0200

    use react-router 4 with browserHistory

--- src/app.jsx
+++ src/app.jsx
@@ -1,18 +1,24 @@
 // Dependencies
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { BrowserRouter as Router, Route, Switch, hashHistory } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
+
+const history = createBrowserHistory();
 
 // Components
 import Index from './components/index.jsx';
 import Thread from './components/thread.jsx';
 import NotFound from './components/404.jsx';
 
-render(
-  <Router history={hashHistory}>
-    <Route path="/" component={Index} />
-    <Route path="/thread/:thread" component={Thread} />
-    <Route path="*" component={NotFound} />
-  </Router>
-  , document.getElementById('app')
+const routes = (
+    <Router history={history}>
+      <Switch>
+          <Route path="/thread/:thread" component={Thread} />
+          <Route path="/" component={Index} />
+          <Route path="*" component={NotFound} />
+      </Switch>
+    </Router>
 );
+
+render(routes, document.getElementById('app'));
