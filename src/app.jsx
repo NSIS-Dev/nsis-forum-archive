6ae50b3045f8c92dd0378d2b12cf43ca28d4e346
Jan T. Sott <jan@idleberg.com>
2017-06-05 15:34:07 +0200

    rename index

--- src/app.jsx
+++ src/app.jsx
@@ -4,15 +4,15 @@ import { render } from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
 
 // Components
-import Home from './components/home.jsx';
+import Index from './components/index.jsx';
 import Thread from './components/thread.jsx';
 import NotFound from './components/404.jsx';
 
-render((
+render(
   <Router history={hashHistory}>
-    <Route path="/" component={Home} />
+    <Route path="/" component={Index} />
     <Route path="/thread/:thread" component={Thread} />
     <Route path="*" component={NotFound} />
   </Router>
-  ), document.getElementById('app')
+  , document.getElementById('app')
 );
