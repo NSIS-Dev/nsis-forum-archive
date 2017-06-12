7664ead09393203ced6a5efda50cc1197a7a72ad
Jan T. Sott <jan@idleberg.com>
2017-06-12 20:51:47 +0200

    indent

--- webpack.config.js
+++ webpack.config.js
@@ -11,14 +11,14 @@ module.exports = {
   target: 'web',
   module: {
     loaders: [
-    {
-      test: /.jsx?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/,
-      query: {
-        presets: ['es2015', 'react']
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
       }
-    }
     ]
   },
   plugins: [
