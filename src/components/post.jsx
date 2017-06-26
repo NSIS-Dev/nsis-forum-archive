4f228a22d9ffefcbdaf92de787315bbc90729a82
Jan T. Sott <jan@idleberg.com>
2017-06-27 00:15:38 +0200

    add onClick event

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -2,6 +2,17 @@
 import React from 'react';
 
 class Post extends React.Component {
+  toggleDateFormat(e) {
+    e.preventDefault();
+
+    let el = e.target || e.srcElement;
+    let htmlText = el.innerHTML;
+    let titleAttr = el.getAttribute("title");
+
+    el.innerHTML = titleAttr;
+    el.setAttribute("title", htmlText);
+  }
+
   render() {
     return (
       <tr id={'post-' + this.props.post.id} key={this.props.post.id}>
@@ -10,7 +21,7 @@ class Post extends React.Component {
         </td>
         <td>
             <span className="text-primary">{this.props.post.user.name || "(deleted user)"}</span>
-            <span className="text-muted small float-right" data-toggle="tooltip" data-placement="top" title={this.props.post.date_string}>{this.props.post.date_relative}</span>
+            <span id="post-date" className="text-muted small float-right"  title={this.props.post.date_string} onClick={this.toggleDateFormat} style={{cursor:'pointer'}}>{this.props.post.date_relative}</span>
             <p dangerouslySetInnerHTML={{ __html: this.props.post.body }}></p>
         </td>
       </tr>
